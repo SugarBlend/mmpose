edde6b98c28f8b6817a1c4fceeacd5db8503546c
SugarBlend <sacha1999b@gmail.com>
2025-10-08 19:18:06 +0300

    Local storage for Label Studio detections has been created. Environment creation, conversion from COCO to LS format.

---
+++ project/annotations/converter.ps1
@@ -0,0 +1,2 @@
+label-studio-converter import coco -i project\dataset\coco_annotations.json -o project\dataset\ls-tasks.json `
+--image-root-url "/data/local-files/?d=images"
---
+++ project/annotations/launch-label-studio.ps1
@@ -0,0 +1,3 @@
+$env:LOCAL_FILES_SERVING_ENABLED = "true"
+$env:LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT = "$pwd\project\dataset"
+label-studio
