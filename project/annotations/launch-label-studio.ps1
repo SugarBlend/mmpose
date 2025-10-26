3b3d28c6d2ddc50aa72dc995b64bce5ad86868df
SugarBlend <sacha1999b@gmail.com>
2025-10-26 20:36:49 +0300

    Extend current labeling ability to use halpe format. Integrate rtmpose to estimate keypoints. Update configs for ls.

--- project/annotations/launch-label-studio.ps1
+++ project/annotations/launch-label-studio.ps1
@@ -1,3 +1,23 @@
+#param(
+#    [Parameter(Mandatory=$true)]
+#    [string]$ZrokUrl
+#)
+#
+#if ($ZrokUrl -match '^https?://') {
+#    $ZrokUrl = $ZrokUrl -replace '^https?://', ''
+#}
+#
+#$ZrokUrl = $ZrokUrl -replace '/+$', ''
+#
+#$env:ALLOWED_HOSTS = "localhost,127.0.0.1,$ZrokUrl"
+#$env:CSRF_TRUSTED_ORIGINS = "https://$ZrokUrl"
+#$env:LABEL_STUDIO_HOST = "https://$ZrokUrl"
+#$env:DEBUG = "true"
+#$env:LOCAL_FILES_SERVING_ENABLED = "true"
+#$env:LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT = "$pwd\project\dataset"
+#label-studio
+
+$env:DEBUG = "true"
 $env:LOCAL_FILES_SERVING_ENABLED = "true"
-$env:LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT = "$pwd\project\dataset"
-label-studio
+$env:LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT = "D:\"
+label-studio start --host localhost --port 8081 --no-browser
--- project/annotations/pose/coco2label_studio.py
+++ project/annotations/pose/coco2label_studio.py
@@ -4,71 +4,66 @@ from pathlib import Path
 from tqdm import tqdm
 from typing import Dict, List, Tuple, Any, Optional
 
-BODY_KEYPOINT_NAMES = {
+BODY_KEYPOINT_COCO_NAMES = {
     0: "nose", 1: "left_eye", 2: "right_eye", 3: "left_ear", 4: "right_ear",
     5: "left_shoulder", 6: "right_shoulder", 7: "left_elbow", 8: "right_elbow",
     9: "left_wrist", 10: "right_wrist", 11: "left_hip", 12: "right_hip",
     13: "left_knee", 14: "right_knee", 15: "left_ankle", 16: "right_ankle"
 }
+BODY_KEYPOINT_HALPE26_NAMES = BODY_KEYPOINT_COCO_NAMES.copy()
+BODY_KEYPOINT_HALPE26_NAMES.update({17: "head", 18: "neck", 19: "hip"})
 
-FOOT_KEYPOINT_NAMES = {
-    0: "left_big_toe", 1: "left_small_toe", 2: "left_heel",
-    3: "right_big_toe", 4: "right_small_toe", 5: "right_heel"
+FOOT_KEYPOINT_WHOLEBODY_NAMES = {
+    17: "left_big_toe", 18: "left_small_toe", 19: "left_heel",
+    20: "right_big_toe", 21: "right_small_toe", 22: "right_heel"
+}
+FOOT_KEYPOINT_HALPE26_NAMES = {
+    20: "left_big_toe", 21: "right_big_toe", 22: "left_small_toe",
+    23: "right_small_toe", 24: "left_heel", 25: "right_heel"
 }
 
-LEFT_HAND_KEYPOINT_NAMES = {
-    0: "left_hand_root", 1: "left_thumb1", 2: "left_thumb2", 3: "left_thumb3", 4: "left_thumb4", 5: "left_forefinger1",
-    6: "left_forefinger2", 7: "left_forefinger3", 8: "left_forefinger4", 9: "left_middle_finger1",
-    10: "left_middle_finger2", 11: "left_middle_finger3", 12: "left_middle_finger4",
-    13: "left_ring_finger1", 14: "left_ring_finger2", 15: "left_ring_finger3", 16: "left_ring_finger4",
-    17: "left_pinky_finger1", 18: "left_pinky_finger2", 19: "left_pinky_finger3", 20: "left_pinky_finger4"
+LEFT_HAND_KEYPOINT_WHOLEBODY_NAMES = {
+    91: "left_hand_root", 92: "left_thumb1", 93: "left_thumb2", 94: "left_thumb3", 95: "left_thumb4",
+    96: "left_forefinger1",
+    97: "left_forefinger2", 98: "left_forefinger3", 99: "left_forefinger4", 100: "left_middle_finger1",
+    101: "left_middle_finger2", 102: "left_middle_finger3", 103: "left_middle_finger4",
+    104: "left_ring_finger1", 105: "left_ring_finger2", 106: "left_ring_finger3", 107: "left_ring_finger4",
+    108: "left_pinky_finger1", 109: "left_pinky_finger2", 110: "left_pinky_finger3", 111: "left_pinky_finger4"
 }
 
-RIGHT_HAND_KEYPOINT_NAMES = {
-   0: "right_hand_root", 1: "right_thumb1", 2: "right_thumb2", 3: "right_thumb3", 4: "right_thumb4",
-    5: "right_forefinger1", 6: "right_forefinger2", 7: "right_forefinger3", 8: "right_forefinger4",
-    9: "right_middle_finger1", 10: "right_middle_finger2", 11: "right_middle_finger3",
-    12: "right_middle_finger4", 13: "right_ring_finger1", 14: "right_ring_finger2", 15: "right_ring_finger3",
-    16: "right_ring_finger4", 17: "right_pinky_finger1", 18: "right_pinky_finger2", 19: "right_pinky_finger3",
-    20: "right_pinky_finger4"
+RIGHT_HAND_KEYPOINT_WHOLEBODY_NAMES = {
+    112: "right_hand_root", 113: "right_thumb1", 114: "right_thumb2", 115: "right_thumb3", 116: "right_thumb4",
+    117: "right_forefinger1", 118: "right_forefinger2", 119: "right_forefinger3", 120: "right_forefinger4",
+    121: "right_middle_finger1", 122: "right_middle_finger2", 123: "right_middle_finger3",
+    124: "right_middle_finger4", 125: "right_ring_finger1", 126: "right_ring_finger2", 127: "right_ring_finger3",
+    128: "right_ring_finger4", 129: "right_pinky_finger1", 130: "right_pinky_finger2", 131: "right_pinky_finger3",
+    132: "right_pinky_finger4"
 }
 
 
-KEYPOINT_CONFIGS = {
-    "body": {
-        "names": BODY_KEYPOINT_NAMES,
-        "from_name": "label_body_keypoints",
-        "prefix": "body"
-    },
-    "face": {
-        "names_func": lambda i: f"face_{i:02d}",
-        "from_name": "label_face_keypoints",
-        "prefix": "face",
-        "valid_key": "face_valid",
-        "kpts_key": "face_kpts"
-    },
-    "left_hand": {
-        "names": LEFT_HAND_KEYPOINT_NAMES,
-        "from_name": "label_lefthand_keypoints",
-        "prefix": "left_hand",
-        "valid_key": "lefthand_valid",
-        "kpts_key": "lefthand_kpts"
-    },
-    "right_hand": {
-        "names": RIGHT_HAND_KEYPOINT_NAMES,
-        "from_name": "label_righthand_keypoints",
-        "prefix": "right_hand",
-        "valid_key": "righthand_valid",
-        "kpts_key": "righthand_kpts"
-    },
-    "foot": {
-        "names": FOOT_KEYPOINT_NAMES,
-        "from_name": "label_foot_keypoints",
-        "prefix": "foot",
-        "valid_key": "foot_valid",
-        "kpts_key": "foot_kpts"
-    }
-}
+def get_keypoint_config(num_joints: int) -> Dict[str, Any]:
+    keypoint_config = {}
+    if num_joints == 26:
+        correspondence = {
+            "body": BODY_KEYPOINT_HALPE26_NAMES,
+            "foot": FOOT_KEYPOINT_HALPE26_NAMES
+        }
+    elif num_joints == 133:
+        correspondence = {
+            "body": BODY_KEYPOINT_COCO_NAMES, "foot": FOOT_KEYPOINT_WHOLEBODY_NAMES,
+            "face": {i: f"face_{i - 23:02d}" for i in range(23, 91)}, "left_hand": LEFT_HAND_KEYPOINT_WHOLEBODY_NAMES,
+            "right_hand": RIGHT_HAND_KEYPOINT_WHOLEBODY_NAMES
+        }
+    else:
+        raise Exception(f"Doesnt found processing for such number of joints: {num_joints}.")
+
+    for part, names in correspondence.items():
+        keypoint_config[part] = {
+            "names": names,
+            "from_name": f"label_{part}_keypoints",
+            "prefix": part
+        }
+    return keypoint_config
 
 
 class KeypointProcessor(object):
@@ -77,7 +72,7 @@ class KeypointProcessor(object):
         self.bbox_id = bbox_id
         self.width = image_info["width"]
         self.height = image_info["height"]
-    
+
     @staticmethod
     def _is_valid_keypoint(kp_x: float, kp_y: float, visibility: float) -> bool:
         return visibility > 0 and kp_x > 0 and kp_y > 0
@@ -85,20 +80,20 @@ class KeypointProcessor(object):
     @staticmethod
     def _is_in_bounds(x_percent: float, y_percent: float) -> bool:
         return 0 <= x_percent <= 100 and 0 <= y_percent <= 100
-    
-    def _to_percent_coords(self, kp_x: float, kp_y: float) -> Tuple[float, float]:
+
+    def _to_percent_coord(self, kp_x: float, kp_y: float) -> Tuple[float, float]:
         x_percent = (kp_x / self.width) * 100
         y_percent = (kp_y / self.height) * 100
         return x_percent, y_percent
-    
+
     def _create_keypoint_annotation(
-        self,
-        x_percent: float,
-        y_percent: float,
-        labels: List[str],
-        point_id: str,
-        from_name: str,
-        width: float = 0.5
+            self,
+            x_percent: float,
+            y_percent: float,
+            labels: List[str],
+            point_id: str,
+            from_name: str,
+            width: float = 0.5
     ) -> Dict[str, Any]:
         return {
             "id": point_id,
@@ -116,64 +111,61 @@ class KeypointProcessor(object):
             "original_height": self.height
         }
 
-    def process_keypoints(
-        self,
-        keypoints: List[float],
-        config: Dict,
-        image_id: int,
-        ann_idx: int,
-        point_ids: Dict
+    def process_joints(
+            self,
+            keypoints: List[float],
+            config: Dict,
+            image_id: int,
+            ann_idx: int,
+            point_ids: Dict
     ) -> List[Dict[str, Any]]:
         results = []
         prefix = config["prefix"]
 
-        for i in range(0, len(keypoints), 3):
-            if i + 2 >= len(keypoints):
-                continue
-
+        for i in range(min(config["names"]) * 3, (max(config["names"]) + 1) * 3, 3):
             point_index = i // 3
             kp_x, kp_y, visibility = keypoints[i], keypoints[i + 1], keypoints[i + 2]
 
             if not self._is_valid_keypoint(kp_x, kp_y, visibility):
                 continue
 
-            x_percent, y_percent = self._to_percent_coords(kp_x, kp_y)
+            x_percent, y_percent = self._to_percent_coord(kp_x, kp_y)
 
             if not self._is_in_bounds(x_percent, y_percent):
                 continue
 
-            if "names" in config:
-                if point_index not in config["names"]:
-                    continue
-                label = config["names"][point_index]
-            else:
-                label = config["names_func"](point_index)
+            if point_index not in config["names"]:
+                continue
+            label = config["names"][point_index]
 
-            point_id = f"{prefix}_{image_id}_{ann_idx}_{point_index:02d}"
-            width = config.get("width", 0.5)
+            point_id = f"{prefix}-{image_id}-{ann_idx}-{point_index:03d}"
 
             annotation = self._create_keypoint_annotation(
-                x_percent, y_percent, [label], point_id,
-                config["from_name"], width
+                x_percent, y_percent, [label], point_id, config["from_name"], config.get("width", 0.5)
             )
             results.append(annotation)
             point_ids[prefix][point_index] = point_id
 
         return results
 
+
 @click.command()
 @click.option("--coco-file", type=str,
-              default="sapiens_coco_wholebody.json", help="Path to file which consider labels in coco format.")
+              default="anns/coco/2025-10-20 12-50-24_halpe-x.json",
+              help="Path to file which consider labels in coco format.")
 @click.option("--output-file", type=str,
-              default="ls-pose-predictions.json", help="Path to dump file.")
+              default="anns/ls/2025-10-20 12-50-24.json", help="Path to dump file.")
 @click.option("--local-storage-path", type=str,
-              default="/data/local-files/?d=images", help="Url for local storage path from Label Studio.")
-@click.option("--separate-threshold", type=int, default=1000, help="Annotations per label studio description file.")
+              default="/data/local-files/?d=NewPoseCustom/2025-10-20 12-50-24",
+              help="Url for local storage path from Label Studio.")
+@click.option("--num-joints", type=int, default=26, help="Number of keypoints per person.")
+@click.option("--frames-per-task", type=int, default=1000, help="Annotations per label studio description file.")
 def coco_wholebody_to_label_studio_predictions(
-    coco_file: str,
-    output_file: str,
-    local_storage_path: str,
-    separate_threshold: Optional[int] = 5
+        coco_file: str,
+        output_file: str,
+        local_storage_path: str,
+        num_joints: int,
+        frames_per_task: Optional[int] = 100
 ) -> List[Dict]:
     coco_file = Path(coco_file)
     output_file = Path(output_file)
@@ -183,13 +175,15 @@ def coco_wholebody_to_label_studio_predictions(
         coco_data = json.load(file)
     image_mapping = {img["id"]: img for img in coco_data["images"]}
 
+    keypoint_config = get_keypoint_config(num_joints)
+
     annotations_by_image = {}
     for ann in coco_data["annotations"]:
         image_id = ann["image_id"]
         annotations_by_image.setdefault(image_id, []).append(ann)
 
     image_ids = list(annotations_by_image.keys())
-    groups = [image_ids[i:i + separate_threshold] for i in range(0, len(image_ids), separate_threshold)]
+    groups = [image_ids[i: i + frames_per_task] for i in range(0, len(image_ids), frames_per_task)]
 
     tasks = []
     for idx, group in tqdm(enumerate(groups), total=(len(groups)), colour="blue", desc="Processing group"):
@@ -216,25 +210,17 @@ def coco_wholebody_to_label_studio_predictions(
                     result.append(bbox_annotation)
 
                 processor = KeypointProcessor(image_info, bbox_id)
-                point_ids = {key: {} for key in KEYPOINT_CONFIGS.keys()}
+                point_ids = {key: {} for key in keypoint_config.keys()}
 
-                for keypoint_type, config in KEYPOINT_CONFIGS.items():
-                    if keypoint_type == "body":
-                        keypoints = ann.get("keypoints", [])
-                        results = processor.process_keypoints(
-                            keypoints, config, image_id, ann_idx, point_ids
-                        )
-                        result.extend(results)
-                    else:
-                        if ann.get(config.get("valid_key", False)):
-                            keypoints = ann.get(config.get("kpts_key", []), [])
-                            results = processor.process_keypoints(
-                                keypoints, config, image_id, ann_idx, point_ids
-                            )
-                            result.extend(results)
+                for keypoint_type, config in keypoint_config.items():
+                    results = processor.process_joints(
+                        ann["keypoints"], config, image_id, ann_idx, point_ids
+                    )
+                    result.extend(results)
             tasks.append(task)
 
-        save_path = output_file.parent.joinpath(f"{output_file.stem}_{idx}.json")
+        save_path = output_file.parent.joinpath(f"{output_file.stem}/{idx}.{len(groups) - 1}-{frames_per_task}.json")
+        save_path.parent.mkdir(parents=True, exist_ok=True)
         with save_path.open("w", encoding="utf-8") as file:
             json.dump(tasks, file, indent=2, ensure_ascii=False)
         tasks.clear()
