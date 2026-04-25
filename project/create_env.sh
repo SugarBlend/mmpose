a3c46387e97178479c28a7116bc040945f464162
SugarBlend <sacha1999b@gmail.com>
2026-04-25 15:32:51 +0300

    Universal setup script

--- project/create_env.sh
+++ project/create_env.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
+
 uv venv --python 3.10
+source .venv/bin/activate
+
+rm -rf ./-DeployAndServe
 git clone https://github.com/SugarBlend/-DeployAndServe.git
+
 uv pip install poetry wheel
-cd "./-DeployAndServe"
-poetry config virtualenvs.create false
+
+cd ./-DeployAndServe
+
+poetry config virtualenvs.create false --local
 poetry build -f wheel
-uv pip install dist/deploy2serve-0.3.0-py3-none-any.whl
+
+uv pip install dist/*.whl
+
 cd ../
+
 uv pip install -r ./project/pyproject.toml --index-strategy unsafe-best-match
 uv pip install -r ./requirements.txt --no-build-isolation
 uv pip install . --no-build-isolation
