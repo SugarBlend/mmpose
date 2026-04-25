462277afa6bbf1a56a30e17eea4070973f722b9f
SugarBlend <sacha1999b@gmail.com>
2026-04-25 13:43:23 +0300

    Update project deps, repair environment creation

--- project/create_env.bat
+++ project/create_env.bat
@@ -1,9 +1,11 @@
 uv venv --python 3.10
 git clone https://github.com/SugarBlend/-DeployAndServe.git
-uv pip install poetry
+uv pip install poetry wheel
 cd "./-DeployAndServe"
-uv run poetry build -f wheel
+poetry config virtualenvs.create false
+poetry build -f wheel
 uv pip install dist/deploy2serve-0.3.0-py3-none-any.whl
 cd ../
-uv pip install -r ./requirements.txt --no-build-isolation
 uv pip install -r ./project/pyproject.toml --index-strategy unsafe-best-match
+uv pip install -r ./requirements.txt --no-build-isolation
+uv pip install . --no-build-isolation
