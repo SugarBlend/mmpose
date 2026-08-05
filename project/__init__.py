1c832919adff1f7eaf354dc03c0d0526cd075b1d
SugarBlend <sacha1999b@gmail.com>
2026-08-05 14:15:33 +0300

    update lost files

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,7 @@
+import torch.multiprocessing as mp
+mp.set_sharing_strategy('file_system')
 import torch
+torch._dynamo.config.optimize_ddp = True
 
 _original_torch_load = torch.load
 def _patched_torch_load(f, *args, **kwargs):
@@ -12,3 +15,5 @@ from .hooks.model_registry import MLflowModelRegistryHook
 from .hooks.safe_mlflow import SafeMLflowVisBackend
 from .hooks.ray import RayReporter
 from .hooks.extend_pck import GroupedPCKAccuracy
+from .hooks.halpe136_metric import Halpe136ToCocoWholeBodyMetric
+from .hooks.compile_model import CompileModelHook
